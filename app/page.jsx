225ef851e7add15b98db1563094f67f5a77e8af5
adp-rgb <fcanirudha@gmail.com>
2026-09-03 23:20:47 +0900

    Delete app directory

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>漢検 Kanji Master</h1>
-      <p>App initialized successfully.</p>
-    </main>
-  );
-}
